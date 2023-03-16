86373bec3c33980e22279bef2fe636e7b9e53004
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-03-15 22:15:57 -0400

    even numbers of digits

--- 2 Trees/129 Sum Root to Leaf Numbers/leet-129-sum-root-to-leaf.js
+++ 2 Trees/129 Sum Root to Leaf Numbers/leet-129-sum-root-to-leaf.js
@@ -41,13 +41,12 @@ Therefore, sum = 495 + 491 + 40 = 1026.
  * @param {TreeNode} root
  * @return {number}
  */
-var sumNumbers = function(root) {
-    
+var sumNumbers = function (root) {
   function traverse(node, num) {
-      if(!node) return null;
-      num += node.val
-      if(!node.left && !node.right) return +num;
-      return traverse(node.left, num) + traverse(node.right, num);
+    if (!node) return null;
+    num += node.val;
+    if (!node.left && !node.right) return +num;
+    return traverse(node.left, num) + traverse(node.right, num);
   }
-  return traverse(root, '');
+  return traverse(root, "");
 };
